986e01eb1a96266d365e3e36afaa704420e6c270
lisas27 <Sahlilisa2000@gmail.com>
2025-07-31 22:33:21 -0500

    main.cs

--- main.cs
+++ main.cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git! Je m'appelle Lisa Sahli");
+        Console.WriteLine("En majuscule " + "Hello, Git! Je m'appelle Lisa Sahli".ToUpper());
     }
 }
